e4c16be04d2750275e459d62e2bb6edecb41df0a
Prabhjot Singh <pjsingh1359@gmail.com>
2022-04-06 22:14:44 -0700

    add nav style

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import _ from 'lodash';
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = "Hello world";
+    
 
-    return element;
-  }
+}
   
   document.body.appendChild(component());
