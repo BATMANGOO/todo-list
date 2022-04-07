11d91d1cae3c2821c1ee7d0024cc8518b59e8eb8
Prabhjot Singh <pjsingh1359@gmail.com>
2022-04-07 12:38:32 -0700

    add form

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import './style.css';
 let app = document.querySelector('#app');
 let navBar = document.querySelector('.nav-bar');
 let addTodoBtn = document.querySelector('.add-todo')
+let form = document.querySelector('form');
+let formContainer = document.querySelector('.form-container');
+let formCancelBtn = document.querySelector('.cancel-button');
 
-console.log({app, navBar, addTodoBtn});
 
-class component {
 
-}
+
+addTodoBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  formContainer.classList.toggle('hide');
+  form.reset();
+});
+
+formCancelBtn.addEventListener('click', (e) => { 
+  e.preventDefault();
+  formContainer.classList.toggle('hide');
+  form.reset();
+});
   
-document.body.appendChild(component());
+// document.body.appendChild(component());
