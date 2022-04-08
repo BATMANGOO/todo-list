082d20c6876bffa5a4c59cb01fcc3ebf9dc5d56e
Prabhjot Singh <pjsingh1359@gmail.com>
2022-04-07 21:50:22 -0700

    saving work

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ let form = document.querySelector('form');
 let formContainer = document.querySelector('.form-container');
 let formCancelBtn = document.querySelector('.cancel-button');
 let todoContainer = document.querySelector('.todo-container');
-
+let projectsContainer = document.querySelector('.project-list');
+// create project form in index.html
 class Project {
   constructor(name) {
     this.name = name;
@@ -23,8 +24,16 @@ class Project {
     return this.todos;
   }
 
-}
-
+  render() {
+    let project = document.createElement('div');
+    project.classList.add('project');
+    project.innerHTML = `
+      <p class="project-name">${this.name}</p>
+      <button class="project-delete">X</button>
+    `;
+    return project;
+  }
+};
 
 class Todo {
     constructor(title, description, dueDate, priority, completed) {
@@ -52,15 +61,15 @@ class Todo {
         `;
         return todo;
     }
-}
+};
 
 let todo1 = new Todo('Buy Milk', 'Buy milk for the family', '2020-01-01', 'High', false);
 let todo2 = new Todo('Buy Bread', 'Buy bread for the family', '2020-01-01', 'High', false);
 
 let project1 = new Project('Project 1');
 project1.addTodo(todo1);
-let project2 = new Project('Project 2');
-project2.addTodo(todo2);
+
+projectsContainer.appendChild(project1.render());
 
 console.log('project 1', project1.showtodos());
 console.log('project 2', project2.showtodos());
