06a6988ee1ff852a67d89584b8c1119645125c17
Prabhjot Singh <pjsingh1359@gmail.com>
2022-04-13 20:55:53 -0700

    add projects todos to populate list

--- src/index.js
+++ src/index.js
@@ -1,106 +1,129 @@
-import _ from 'lodash';
+import _, { create, uniqueId } from 'lodash';
 import './style.css';
 
-let app = document.querySelector('#app');
-let navBar = document.querySelector('.nav-bar');
 let addTodoBtn = document.querySelector('.add-todo')
-let form = document.querySelector('form');
+let createTodoBtn = document.querySelector('.create-todo');
+let projectFormBtn = document.querySelector('.add-project');
+let createProjBtn = document.querySelector('.create-project');
+let projectFormContainer = document.querySelector('.proj-form-container');
+let projectForm = document.querySelector('.project-form');
+let form = document.querySelector('.todo-form');
 let formContainer = document.querySelector('.form-container');
 let formCancelBtn = document.querySelector('.cancel-button');
 let todoContainer = document.querySelector('.todo-container');
 let projectsContainer = document.querySelector('.project-list');
 let projects = projectsContainer.childNodes;
+let projects2 = document.getElementsByClassName('project');
+let projectFormName = document.querySelector('.project-form-name');
 
-// create project form in index.html
-class Project {
-  constructor(name) {
-    this.name = name;
-    this.todos = [];
+// console.log({projects});
+
+let projectsArray = [];
+let currentProject = null;
+
+const Project = (name) => {
+  const project = {};
+  project.id = _.uniqueId();
+  project.name = name;
+  project.todos = [];
+
+  project.addTodo = (todo) => {
+    project.todos.push(todo);
+  };
+
+  project.getTodos = () => {
+    return project.todos;
   }
 
-  addTodo(todo) {
-      this.todos.push(todo);
+  project.getId = () => {
+    return project.id;
+  }
+
+  project.renderTodos = () => {
+    let todos = project.getTodos();
+    todoContainer.innerHTML = '';
+    if(todos.length <= 0) {
+      return todoContainer.innerHTML = `<p>No todos for this project</p>`;
     }
-
-  showtodos() {
-    return this.todos;
-  }
-
-  render() {
-    let project = document.createElement('div');
-    project.classList.add('project');
-    project.innerHTML = `
-      <p class="project-name">${this.name}</p>
-      <button class="project-delete">X</button>
-    `;
-    return project;
-  }
-};
-
-class displayTodos extends Project {
-  constructor(todos) {
-    super(todos);
-    this.todos = todos;
-  }
-
-  render() {
-    this.todos.forEach(todo => {
-      todo.style.width = '300px';
-      todo.innerHTML = `
-      <div class='todo-item'>
-        <p class="todo-name">${this.title}</p>
-        <p class="todo-description">${this.description}</p>
-        <p class="todo-date">${this.dueDate}</p>
-        <p class="todo-priority">${this.priority}</p>
-        <div class="todo-buttons">
-          <button class="edit-todo">Edit</button>
-          <button class="delete-todo">Delete</button>
-          <button class="check-todo">Check</button>
-        </div>
-      </div>
-      `;
+    return todos.forEach(todo => {
+      todoContainer.appendChild(todo.render());
     });
   }
- }
 
-class Todo {
-    constructor(title, description, dueDate, priority, completed) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.completed = completed;
-    }
+  project.render = () => {
+    projectsContainer.innerHTML += `
+      <div data-id=${project.id} class="project project-name">${project.name}<button class="project-delete">X</button></div>
+    `;
+    return projectsContainer;
+  }
 
-    render() {
-        let todo = document.createElement('div');
-        todo.classList.add('todo-item');
-        todo.style.width = '300px';
-        todo.innerHTML = `
-            <p class="todo-name">${this.title}</p>
-            <p class="todo-description">${this.description}</p>
-            <p class="todo-date">${this.dueDate}</p>
-            <p class="todo-priority">${this.priority}</p>
-            <div class="todo-buttons">
-              <button class="edit-todo">Edit</button>
-              <button class="delete-todo">Delete</button>
-              <button class="check-todo">Check</button>
-            </div>
-        `;
-        return todo;
-    }
+  return project
 };
 
-let todo1 = new Todo('Buy Milk', 'Buy milk for the family', '2020-01-01', 'High', false);
+const Todo = (title, description, dueDate, priority, completed) => {
+  const todo = {};
+  todo.title = title;
+  todo.description = description;
+  todo.dueDate = dueDate;
+  todo.priority = priority;
+  todo.completed = completed;
 
-let project1 = new Project('Project 1');
+  todo.render = () => {
+    let todoContainer = document.createElement('div');
+    todoContainer.classList.add('todo-item');
+    todoContainer.style.width = '300px';
+    todoContainer.innerHTML = `
+      <p class="todo-name">${todo.title}</p>
+      <p class="todo-description">${todo.description}</p>
+      <p class="todo-date">${todo.dueDate}</p>
+      <p class="todo-priority">${todo.priority}</p>
+      <div class="todo-buttons">
+        <button class="edit-todo">Edit</button>
+        <button class="delete-todo">Delete</button>
+        <button class="check-todo">Check</button>
+      </div>
+    `;
+    return todoContainer;
+  }
+  return todo;
+};
+
+let todo1 = Todo('Buy Milk', 'Buy milk for the family', '2020-01-01', 'High', false);
+
+let project1 = Project('Family time');
+projectsArray.push(project1);
+console.log(projectsArray);
 project1.addTodo(todo1);
 
-projectsContainer.appendChild(project1.render());
+// console.log(projects)
+project1.render();
 
 projects.forEach(project => {
-  project.addEventListener('click', e => {
-    console.log(e.target);
+  project.addEventListener('click', (e) => {
+    if (e.target.classList.contains('project-delete')) {
+      projectsContainer.removeChild(project);
+    }
+    // if () {
+    //   console.log('project exists');
+    // } else {
+    //   console.log('project does not exist');
+    // }
+    projectsArray.find(project => {
+      if (project.getId() === e.target.getAttribute('data-id')) {
+        console.log('project exists');
+        currentProject = project;
+        currentProject.renderTodos();
+      } else {
+        console.log('project does not exist');
+      }
+    })
+    // if(projectsArray.includes(e.target.getAttribute('data-id'))) {
+    //   console.log('project exists');
+    // } else {
+    //   console.log('project does not exist');
+    // }
+    console.log(e.target.getAttribute('data-id'));
+
   })
 });
 
@@ -110,6 +133,42 @@ addTodoBtn.addEventListener('click', (e) => {
   form.reset();
 });
 
+projectFormBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  projectFormContainer.classList.toggle('hide2');
+});
+
+createProjBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let projectName = projectForm.elements.name.value;
+  let project = Project(projectName);
+  projectsArray.push(project);
+  project.render();
+  console.log(projectsArray);
+  currentProject = project;
+  currentProject.renderTodos();
+  console.log({currentProject})
+  projectForm.reset();
+  projectFormContainer.classList.toggle('hide2');
+});
+
+createTodoBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let title = form.elements.title.value;
+  let description = form.elements.description.value;
+  let dueDate = form.elements.date.value;
+  let priority = form.elements.priority.value;
+  let completed = false;
+  console.log({ title, description, dueDate, priority, completed });
+  let todo = Todo(title, description, dueDate, priority, completed);
+  let project = currentProject.addTodo(todo);
+  currentProject.renderTodos();
+  form.reset();
+  formContainer.classList.toggle('hide');
+});
+
+
+
 formCancelBtn.addEventListener('click', (e) => { 
   e.preventDefault();
   formContainer.classList.toggle('hide');
@@ -118,4 +177,4 @@ formCancelBtn.addEventListener('click', (e) => {
   
 // document.body.appendChild(component());
 
-console.log(projects);
+// console.log(projects);
